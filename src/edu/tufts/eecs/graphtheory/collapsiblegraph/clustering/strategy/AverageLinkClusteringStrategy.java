0f431fb0d40909c0ee72e8535f131f8e5c936403
jeremy <jeremy@192.168.1.147>
2011-05-30 23:24:36 -0400

    Added an Average Link ClusteringStrategy

---
+++ src/edu/tufts/eecs/graphtheory/collapsiblegraph/clustering/strategy/AverageLinkClusteringStrategy.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.tufts.eecs.graphtheory.collapsiblegraph.clustering.strategy;
+
+import edu.tufts.eecs.graphtheory.collapsiblegraph.node.Node;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * A ClusteringStrategy using the Complete Link method of clustering,
+ * wherein the distance between two clusters is defined as the maximum distance
+ * between any node in one cluster and any node in the other.
+ * @author jeremy
+ */
+public class AverageLinkClusteringStrategy extends AbstractClusteringStrategy {
+
+    @Override
+    protected double findDistance(int firstDendrogramNodeIndex, int secondDendrogramNodeIndex,
+            List<Set<Node>> currentClusters) {
+
+        double sum = 0.0;
+        int comparisons = 0;
+        for (Node currentFirstDendrogramNode : currentClusters.get(firstDendrogramNodeIndex)) {
+            for (Node currentSecondDendrogramNode : currentClusters.get(secondDendrogramNodeIndex)) {
+                double thisDistance = currentFirstDendrogramNode.getDistance(currentSecondDendrogramNode);
+                sum += thisDistance;
+                comparisons++;
+            }
+        }
+        return sum / comparisons;
+    }
+}
