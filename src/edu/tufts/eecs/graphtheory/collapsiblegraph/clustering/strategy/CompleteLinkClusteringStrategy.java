c7ef12fd4a6ca55f8f7c5b820af01eba9f0a132b
Jeremy Freeman <jeremy.freeman@gmail.com>
2011-06-19 22:51:34 -0400

    Removed unused imports

--- src/edu/tufts/eecs/graphtheory/collapsiblegraph/clustering/strategy/CompleteLinkClusteringStrategy.java
+++ src/edu/tufts/eecs/graphtheory/collapsiblegraph/clustering/strategy/CompleteLinkClusteringStrategy.java
@@ -1,12 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package edu.tufts.eecs.graphtheory.collapsiblegraph.clustering.strategy;
 
 import edu.tufts.eecs.graphtheory.collapsiblegraph.node.Node;
 import java.util.List;
-import java.util.Set;
 
 /**
  * A ClusteringStrategy using the Complete Link method of clustering,
