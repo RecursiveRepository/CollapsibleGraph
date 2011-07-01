ae479a644e8a22fd4f792056aa2267baad1a799c
Jeremy Freeman <jeremy.freeman@gmail.com>
2011-07-01 15:17:46 -0400

    Dropped the old DendrogramEdge[] array that was originally part of this classes payload. Tried to make it serializable.

--- src/edu/tufts/eecs/graphtheory/collapsiblegraph/clustering/Dendrogram.java
+++ src/edu/tufts/eecs/graphtheory/collapsiblegraph/clustering/Dendrogram.java
@@ -1,15 +1,17 @@
 package edu.tufts.eecs.graphtheory.collapsiblegraph.clustering;
 
-/**
+import java.io.Serializable;
+
+/*.eecs
  *
  * @author Jeremy
  */
-public class Dendrogram {
+public class Dendrogram implements Serializable {
+    
     private DendrogramNode rootNode;
     private DendrogramEdge rootEdge;
     
-    private DendrogramEdge[] dendrogramEdges;
-    
+   
     public Dendrogram (DendrogramNode rootNode, DendrogramEdge rootEdge) {
         this.rootNode = rootNode;
         this.rootEdge = rootEdge;
@@ -21,14 +23,6 @@ public class Dendrogram {
     
     public DendrogramEdge getRootEdge() {
         return rootEdge;
-    }
-    
-    public DendrogramEdge[] getDendrogramEdges() {
-        return dendrogramEdges;
-    }
-    
-    public void setDendrogramEdges(DendrogramEdge[] dendrogramEdges) {
-        this.dendrogramEdges = dendrogramEdges;
     }
     
 }
